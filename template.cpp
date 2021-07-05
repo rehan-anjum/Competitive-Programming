fff3428ac5f1190ee598eb5889534090b21a1fa6
Rehan Anjum <75551393+rehan-anjum@users.noreply.github.com>
2021-07-05 16:07:15 -0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize "unroll-loops"
+#pragma GCC optimize "prefetch-loop-arrays"
+#pragma GCC target "sse,sse2,sse3,sse4,abm,avx,mmx,popcnt,avx2,fma"
+#include <bits/stdc++.h> 
+using namespace std;
+using li = long int;
+using ll = long long int;
+using ull = unsigned long long int;
+using pii = pair<int,int>;
+using pcc = pair<char,char>;
+using plli = pair<ll,ll>;
+using vi = vector<int>;
+using vpii = vector<pii>;
+#define pb push_back
+#define mp make_pair
+#define trav(a,x) for (auto &a : x)
+#define INF 0x3f3f3f3f
+#define db1 cout << "db1" << endl;
+#define db2 cout << "db2" << endl;
+
+const int MX = 1e5+3;
+int w,n,cnt;
+int c[MX];
+
+int main() {                
+
+    cin.tie(0)->sync_with_stdio(0);
+
+
+}
